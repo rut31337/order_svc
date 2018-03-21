8207a8036949b8bbb9f71172e8ee07b7270a2d66
Patrick Rutledge <prutledg@redhat.com>
2018-03-21 13:22:23 -0400

    added support for bad ssl certs

--- order_svc.sh
+++ order_svc.sh
@@ -14,14 +14,15 @@ apiWait=3 # Seconds between API calls in a group
 # Dont touch from here on
 
 usage() {
-  echo "Error: Usage $0 -c <catalog name> -i <item name> [ -u <username> -P <password> -t <totalRequests> -g <groupCount> -p <groupWait> -a <apiWait> -w <uri> -d <key1=value;key2=value> -n ]"
+  echo "Error: Usage $0 -c <catalog name> -i <item name> [ -u <username> -P <password> -t <totalRequests> -g <groupCount> -p <groupWait> -a <apiWait> -w <uri> -d <key1=value;key2=value> -n -N]"
 }
 
-while getopts nu:P:c:i:t:g:p:a:w:d: FLAG; do
+while getopts Nnu:P:c:i:t:g:p:a:w:d: FLAG; do
   case $FLAG in
     n) noni=1;;
     u) username="$OPTARG";;
     P) password="$OPTARG";;
+    N) insecure=1;;
     c) catalogName="$OPTARG";;
     i) itemName="$OPTARG";;
     t) totalRequests="$OPTARG";;
@@ -54,13 +55,30 @@ then
   echo
 fi
 
-tok=`curl -s --user $username:$password -X GET -H "Accept: application/json" $uri/api/auth|python -m json.tool|grep auth_token|cut -f4 -d\"`
-#echo "tok is $tok"
+if [ "$insecure" == 1 ]
+then
+  ssl="-k"
+else
+  ssl=""
+fi
+
+tok=`curl -s $ssl --user $username:$password -X GET -H "Accept: application/json" $uri/api/auth`
+err=$?
+tt=`echo $tok|grep error`
+if [ $err != 0 -o -n "$tt" ]
+then
+  echo "ERROR: Authentication failed to CloudForms."
+  echo "$tok"
+  exit $err
+fi
+tok=`echo $tok|python -m json.tool|grep auth_token|cut -f4 -d\"`
+#echo "tok is '$tok'"
+
 catalogName=`echo $catalogName|sed "s/ /+/g"`
 itemName=`echo $itemName|sed "s/ /+/g"`
-catalogID=`curl -s -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X GET "$uri/api/service_catalogs?attributes=name,id&expand=resources&filter%5B%5D=name%3D$catalogName" | python -m json.tool |grep '"id"' | cut -f2 -d:|sed "s/[ ,]//g"`
+catalogID=`curl -s $ssl -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X GET "$uri/api/service_catalogs?attributes=name,id&expand=resources&filter%5B%5D=name%3D$catalogName" | python -m json.tool |grep '"id"' | cut -f2 -d:|sed "s/[ ,]//g"`
 echo "catalogID is $catalogID"
-itemID=`curl -s -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X GET "$uri/api/service_templates?attributes=service_template_catalog_id,id,name&expand=resources&filter%5B%5D=name=$itemName&filter%5B%5D=service_template_catalog_id%3D$catalogID" | python -m json.tool |grep '"id"' | cut -f2 -d:|sed "s/[ ,]//g"`
+itemID=`curl -s $ssl -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X GET "$uri/api/service_templates?attributes=service_template_catalog_id,id,name&expand=resources&filter%5B%5D=name=$itemName&filter%5B%5D=service_template_catalog_id%3D$catalogID" | python -m json.tool |grep '"id"' | cut -f2 -d:|sed "s/[ ,]//g"`
 echo "itemID is $itemID"
 
 if [ "$noni" != 1 ]
@@ -92,11 +110,11 @@ g=1
 while [ $t -le $totalRequests ]
 do
   c=1
-  tok=`curl -s --user $username:$password -X GET -H "Accept: application/json" $uri/api/auth|python -m json.tool|grep auth_token|cut -f4 -d\"`
+  tok=`curl -s $ssl --user $username:$password -X GET -H "Accept: application/json" $uri/api/auth|python -m json.tool|grep auth_token|cut -f4 -d\"`
   while [ $c -le $groupCount -a $t -le $totalRequests ]
   do
     echo "Deploying request $t in group $g"
-    out=`curl -s -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X POST $uri/api/service_catalogs/$catalogID/service_templates -d "$PAYLOAD" | python -m json.tool`
+    out=`curl -s $ssl -H "X-Auth-Token: $tok" -H "Content-Type: application/json" -X POST $uri/api/service_catalogs/$catalogID/service_templates -d "$PAYLOAD" | python -m json.tool`
     (( c = $c + 1 ))
     (( t = $t + 1 ))
     sleep $apiWait
